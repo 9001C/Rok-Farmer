fab5f7813a16882dd394298ae533b58752225152
9001C <00swipe@gmail.com>
2024-06-20 11:26:44 -0700

    Create Farm.py

---
+++ Farm.py
@@ -0,0 +1,39 @@
+import pyautogui
+import time
+import keyboard
+import mouse 
+import cv2
+#import argparser thing
+
+#argparse arguments for type
+
+#DON'T RUN 24/7
+BasePath = "" # file path to the image folder
+
+RandomClickRange = 1 # amount of variance allowed in each click to avoid bot detection  
+RandomTimeRange = 1 #Time variance to avoid bot detection 
+
+
+
+def clearScreen:
+  #search for x
+  #check if in city
+  
+def RandomAction:
+  #random action to simulate a real human playing 
+
+def farm(type):
+  #check if in or out of city then push city button
+  #click search button
+  if(type == 1):
+    #search for food
+  elif(type == 2):
+    #search for wood
+  elif(type == 3):
+    #search for stone 
+  elif(type == 4):
+    #search for gold
+    else:
+      print(type + " not found")
+  
+    
